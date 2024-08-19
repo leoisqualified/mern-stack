e7848701943509fcd53fc80cc5d1ba414cd5c6de
andy <eaadenutsi@st.ug.edu.gh>
2024-08-19 15:28:15 +0000

    added mongodb

--- todolist/src/components/Form.jsx
+++ todolist/src/components/Form.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
 const Form = () => {
-    return ( 
-        <form className="form">
-            <input className="input" placeholder="Enter your reminder! :D"/>
+    const [text, setText] = useState("");
+
+    const onInputChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault(); // Prevent the default form submission behavior
+        // You can add your form submission logic here
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                className="input"
+                placeholder="Enter your reminder! :D"
+                onChange={onInputChange}
+                value={text} // Bind the input value to state
+            />
         </form>
-     );
-}
- 
+    );
+};
+
 export default Form;
